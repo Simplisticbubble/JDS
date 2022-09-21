182eeb3747084fdb2af7395e56403ac64a91a94c
kevin <ks.kevinseu@gmail.com>
2022-09-21 13:22:11 +1000

    created a recipients class and list

--- jds.py
+++ jds.py
@@ -3,8 +3,38 @@ import schedule
 import time
 import smtplib, ssl
 
+class Person:
+    def __init__(self, name, email, dashBoard):
+        self.name = name
+        self.email = email
+        self.dashBoard = dashBoard
 
-def postReq():
+    def printDetail(abc):
+        print("User: " + abc.name)
+        print("Email: " + abc.email)
+        print("DashBoard: " + abc.dashBoard)
+    def getEmail(abc):
+        return abc.email
+
+    def hasdash(abc,dboard):
+        if dboard == abc.dashBoard:
+            return True
+
+"List of Recipients"
+ListRec = []
+ListRec.append(Person("John", "ks.kevinseu@gmail.com", "MzM5MzExMHxWSVp8REFTSEJPQVJEfDU2MTkwMjU"))
+ListRec.append(Person("Kevin", "electrobubz@gmail.com", "MzM5MzExMHxWSVp8REFTSEJPQVJEfDU2MTkwMjU"))
+ListRec.append(Person("Jimmy", "Jimmy.test@gmail.com", "Business"))
+
+def ListAppend(name, email,dashBoard):
+    ListRec.append(Person(name, email, dashBoard))
+
+def printList():
+    for i in ListRec:
+        i.printDetail()
+    
+
+def postReq(abc):
 
     url = 'https://api.newrelic.com/graphql'
     myobj = {"query":"mutation {\n dashboardCreateSnapshotUrl(guid:\"MzM5MzExMHxWSVp8REFTSEJPQVJEfDU2MTkwMjU\")\n}\n", "variables":""}
@@ -16,15 +46,12 @@ def postReq():
 
     #print the response text (the content of the requested file):
 
-    
-    
     return(x.text);
 
 
 
 def slicer(pdfFile):
 
-    
     return(pdfFile[39:121])
 
 
@@ -40,20 +67,25 @@ password = "fxfynlpbuyubncgv"
 
 # Create a secure SSL context
 context = ssl.create_default_context()
-def sendMail():
-    print(slicer(postReq()))
-    SUBJECT = "TEST"
-    TEXT = slicer(postReq())
-    message = """\
-    Subject: %s
+def sendMail(abc):
 
-    %s
-    """ % ( SUBJECT, TEXT)
-    
-    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-        server.login("python.test.123454321@gmail.com", password)
-        server.sendmail(sender_email, receiver_email, message)
+    for i in ListRec:
+        if i.hasdash(abc):
+            receiver_email = i.getEmail()
+            print(slicer(postReq(abc)))
+            SUBJECT = "TEST"
+            TEXT = slicer(postReq(abc))
+            message = """\
+            Subject: %s
+
+            %s
+            """ % ( SUBJECT, TEXT)
+            
+            with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+                server.login("python.test.123454321@gmail.com", password)
+                server.sendmail(sender_email, receiver_email, message)
 
 
+schedule.every().day.at("10:30").do(sendMail("MzM5MzExMHxWSVp8REFTSEJPQVJEfDU2MTkwMjU"))
+sendMail("MzM5MzExMHxWSVp8REFTSEJPQVJEfDU2MTkwMjU")
 
-sendMail()
